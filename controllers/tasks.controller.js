a5ddb66bf1e20644dbbec317fa84bebf8065869f
Duck1s <joeri.breedveld123@gmail.com>
2022-06-02 20:07:53 +0200

    feat: add all controller functions and add find functionality

---
+++ controllers/tasks.controller.js
@@ -0,0 +1,37 @@
+const Task = require('../models/tasks.model')
+const asyncWrapper = require('express-async-wrapper')
+
+const getAllTasks = asyncWrapper(async (req, res) => {
+	const tasks = await Task.find({})
+})
+
+const createTask = asyncWrapper(async (req, res) => {
+	const task = await Task.create(req.body)
+})
+
+const getTask = asyncWrapper(async (req, res, next) => {
+	const { id: taskID } = req.params
+	const task = await Task.findOne({ _id: taskID })
+})
+
+const deleteTask = asyncWrapper(async (req, res, next) => {
+	const { id: taskID } = req.params
+	const task = await Task.findOneAndDelete({ _id: taskID })
+})
+
+const updateTask = asyncWrapper(async (req, res, next) => {
+	const { id: taskID } = req.params
+
+	const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+		new: true,
+		runValidators: true,
+	})
+})
+
+module.exports = {
+	getAllTasks,
+	createTask,
+	getTask,
+	updateTask,
+	deleteTask,
+}
