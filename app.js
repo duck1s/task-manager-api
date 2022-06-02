c4ddd4e71669ce22131819d58b0469565499e91d
Duck1s <joeri.breedveld123@gmail.com>
2022-06-02 19:57:05 +0200

    feat: setup appjs with router, server and dotenv variables

---
+++ app.js
@@ -0,0 +1,18 @@
+// Config
+const express = require('express')
+require('dotenv').config()
+const tasksRouter = require('./routes/tasks.route')
+
+const app = express()
+
+app.use(express.json())
+
+// Routes
+app.use('/api/v1/tasks', tasksRouter)
+
+// Server
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+	console.log(`Server is listening on ${port}`)
+})
